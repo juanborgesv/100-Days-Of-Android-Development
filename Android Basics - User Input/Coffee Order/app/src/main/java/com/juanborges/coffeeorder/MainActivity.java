a39174a5890b4fcb2c3bbfce2c2139d5da102874
Juan Borges <juan.vegaborges@gmail.com>
2018-07-16 23:00:15 -0400

    Coffe-order App finished

--- Android Basics - User Input/Coffee Order/app/src/main/java/com/juanborges/coffeeorder/MainActivity.java
+++ Android Basics - User Input/Coffee Order/app/src/main/java/com/juanborges/coffeeorder/MainActivity.java
@@ -3,20 +3,26 @@ package com.juanborges.coffeeorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-    Button decreaseButton;
-    Button increaseButton;
-    TextView quantityTextView;
-    TextView totalTextView;
+    float[] coffeeValue = {3.45f, 3.45f, 4.05f, 1.95f, 4.25f};
+    int[] iCoffeeQuantity = {0, 0, 0, 0, 0};
+    TextView[] coffeeQuantity = new TextView[5];
+    String[] coffeeName = {"Latte", "Cappuccino", "Flat White", "Espresso", "Caramel Macchiato"};
 
-    int quantity;
-    float coffeeValue;
-    float total;
+    EditText firstName;
+    EditText lastName;
+
+    TextView orderSummary;
+
+    CheckBox whipped;
+    CheckBox caramel;
+    CheckBox chocolate;
+    CheckBox nutella;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,48 +30,87 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        decreaseButton = findViewById(R.id.decrease_button);
-        increaseButton = findViewById(R.id.increase_button);
-        quantityTextView = findViewById(R.id.qvalue_tv);
-        totalTextView = findViewById(R.id.total_tv);
+        TextView myTextView = findViewById(R.id.latte_quantity_text);
+        coffeeQuantity[0] = myTextView;
+        myTextView = findViewById(R.id.cappuccino_quantity_text);
+        coffeeQuantity[1] = myTextView;
+        myTextView = findViewById(R.id.flatwhite_quantity_text);
+        coffeeQuantity[2] = myTextView;
+        myTextView = findViewById(R.id.espresso_quantity_text);
+        coffeeQuantity[3] = myTextView;
+        myTextView = findViewById(R.id.caramel_quantity_text);
+        coffeeQuantity[4] = myTextView;
 
-        quantity = Integer.parseInt(quantityTextView.getText().toString());
-        coffeeValue = 2.75f;
-        total = quantity*coffeeValue;
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+
+        orderSummary = findViewById(R.id.summary_text);
+
+        whipped = findViewById(R.id.whipped_cb);
+        caramel = findViewById(R.id.caramel_cb);
+        chocolate = findViewById(R.id.chocolate_cb);
+        nutella = findViewById(R.id.nutella_cb);
     }
 
-    public void decreaseValue(View view)
+    public void decreaseQuantity(View view)
     {
-        if (quantity > 0)
+        int coffeeTag = Integer.parseInt(view.getTag().toString());
+        if (iCoffeeQuantity[coffeeTag] > 0)
         {
-            quantityTextView.setText("" + --quantity);
-            calculatePrice();
+            --iCoffeeQuantity[coffeeTag];
+            coffeeQuantity[coffeeTag].setText("" + iCoffeeQuantity[coffeeTag]);
         }
     }
 
-    public void increaseValue(View view)
+    public void increaseQuantity(View view)
     {
-        quantityTextView.setText(""+ ++quantity);
-        calculatePrice();
+        int coffeeTag = Integer.parseInt(view.getTag().toString());
+        ++iCoffeeQuantity[coffeeTag];
+        coffeeQuantity[coffeeTag].setText("" + iCoffeeQuantity[coffeeTag]);
     }
 
-    void calculatePrice()
+    public void orderSummary(View view)
     {
-        total = quantity*coffeeValue;
-        totalTextView.setText("Total: $"+ String.format("%.2f", total));
-    }
+        String summary = "" + firstName.getText().toString() + " " + lastName.getText().toString() +
+                "\n\n";
+        float price;
+        float total = 0;
+        for (int x = 0; x < 5; x++)
+        {
+            if (iCoffeeQuantity[x] > 0)
+            {
+                price = iCoffeeQuantity[x] * coffeeValue[x];
+                summary += iCoffeeQuantity[x];
+                summary += "  " + coffeeName[x];
+                summary += "\t\t $" + price;
+                summary += "\n\n";
 
-    public void displayToast(View view)
-    {
-        if (quantity > 0)
-        {
-            Toast.makeText(MainActivity.this, "Your order has been registered.",
-                    Toast.LENGTH_LONG).show();
+                total += price;
+            }
         }
-        else
+
+        if (whipped.isChecked())
         {
-            Toast.makeText(MainActivity.this,"Error: Set one coffee or more to order.",
-                    Toast.LENGTH_LONG).show();
+            summary += "Topping: Whipped Cream \t\t $1.00\n";
+            total += 1.00f;
         }
+        if (caramel.isChecked())
+        {
+            summary += "Topping: Caramel Syrup \t\t $1.00\n";
+            total += 1.00f;
+        }
+        if (chocolate.isChecked())
+        {
+            summary += "Topping: Chocolate \t\t $1.00\n";
+            total += 1.00f;
+        }
+        if (nutella.isChecked())
+        {
+            summary += "Topping: Nutella \t\t $1.00\n";
+            total += 1.00f;
+        }
+
+        summary += "Total: $" + total + "\n\n";
+        orderSummary.setText("" + summary);
     }
 }
