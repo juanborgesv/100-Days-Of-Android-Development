e3db8c00739ca6deee06ec8edfdc47d438ced9e8
Juan Borges <juan.vegaborges@gmail.com>
2018-07-27 23:18:34 -0400

    Minor updates

--- Android Basics - User Input/Miwok/app/src/main/java/com/juanborges/miwok/Word.java
+++ Android Basics - User Input/Miwok/app/src/main/java/com/juanborges/miwok/Word.java
@@ -1,12 +1,56 @@
 package com.juanborges.miwok;
 
-
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn its Miwok translation.
+ * It contains a default translation and a Miwok translation for that word.
+ */
 public class Word {
+
+    // Default translation of the word
     String defaultTranslation;
+
+    // Miwok translation of the word
     String miwokTranslation;
 
-    public Word(String defaultT, String miwokT) {
-        defaultTranslation = defaultT;
-        miwokTranslation = miwokT;
+    /**
+     * Constructor of the class. Set the Default and Miwok translation of the Word.
+     * @param mDefaultTranslation
+     * @param mMiwokTranslation
+     */
+    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+        defaultTranslation = mDefaultTranslation;
+        miwokTranslation = mMiwokTranslation;
+    }
+
+    /**
+     * Get the Default translation
+     * @return defaultTranslation
+     */
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation
+     * @return miwokTranslation
+     */
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+
+    /**
+     * Set the Default translation
+     * @param mDefaultTranslation
+     */
+    public void setDefaultTranslation(String mDefaultTranslation) {
+        defaultTranslation = mDefaultTranslation;
+    }
+
+    /**
+     * Set the Miwok translation
+     * @param mMiwokTranslation
+     */
+    public void setMiwokTranslation(String mMiwokTranslation){
+        miwokTranslation = mMiwokTranslation;
     }
 }
