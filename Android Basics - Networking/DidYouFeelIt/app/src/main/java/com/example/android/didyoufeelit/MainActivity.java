6fbcd5736568057fbc65a76076b87bb4929510cb
Juan Borges <juan.vegaborges@gmail.com>
2018-08-18 23:41:39 -0400

    DidYouFeelIt App Fixed

--- Android Basics - Networking/DidYouFeelIt/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ Android Basics - Networking/DidYouFeelIt/app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,24 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -25,20 +40,27 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /** URL for earthquake data from the USGS dataset */
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+
+    /**
+     * Tag for log messages
+     */
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        // Create an {@link AsyncTask} to perform the HTTP request to the given URL
+        // on a background thread. When the result is received on the main UI thread,
+        // then update the UI.
+        EarthquakeTask task = new EarthquakeTask();
+        task.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +75,26 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeTask extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null)
+                return null;
+
+            // Perform the HTTP request for earthquake data and process the response.
+            return Utils.fetchEarthquakeData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            // If there is no result, do nothing.
+            if (event == null)
+                return;
+
+            updateUi(event);
+        }
     }
 }
